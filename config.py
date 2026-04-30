27c78a94f79992c87303aeb130606f4e3b4efa79
ahmedh281 <dr.ahmed.hafez281@gmail.com>
2026-04-30 12:24:44 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,2 +1,2 @@
-#API_KEY = "AIzaSyAUIjLcEdDGAemE76dxqusIx_rcO8mFadQ"
-API_KEY = "AIzaSyDioVlyWQOdGtOfzwBCzjEqkEY2YXBud28"
+API_KEY = "AIzaSyAUIjLcEdDGAemE76dxqusIx_rcO8mFadQ"
+#API_KEY = "AIzaSyDioVlyWQOdGtOfzwBCzjEqkEY2YXBud28"
